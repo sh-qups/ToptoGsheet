80c5a362f0822839d9085dc157bc9ed5b044b160
shashwata <sh.qups@gmail.com>
2021-09-19 08:03:53 +0600

    added requirements

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
+import subprocess
 import os
 from utils.gspread_utils import *
 from datetime import datetime
 from pathlib import Path
+import pandas as pd
 from time import sleep
 
 json_file = Path(__file__).parent / 'credentials/credentials.json'
@@ -10,8 +12,10 @@ json_file = Path(__file__).parent / 'credentials/credentials.json'
 def read_date_time_underscore_format():
     return str(datetime.now().strftime('%d-%m-%y_%H-%M-%S'))
 
+
 def hour_min_time_underscore_format():
     return str(datetime.now().strftime('%H-%M'))
+
 
 def create_gsheet():
     gsheet_name = f'LR_MCR_{read_date_time_underscore_format()}'
@@ -19,23 +23,34 @@ def create_gsheet():
     return gsheet_name
 
 
+script = os.path.abspath(Path(__file__).parent / 'top.sh')
+subprocess.call(script)
+
 gsheet_name = ''
-for j in range(1, 5):
-    command = 'top -b -n 1 > top.txt'
-    os.system(command)
-    # txt to df
-    file1 = open('top.txt', 'r')
-    Lines = file1.readlines()
+for k in range(1, 5000):
     result = []
-    for i in range(7, len(Lines)):
-        result_list = (Lines[i].split('\n'))[0].split(' ')
-        result_list = list(filter(lambda a: a != '', result_list))
-        result.append(result_list)
-    df = pd.DataFrame(result, columns=['PID', 'USER', 'PR', 'NI', 'VIRT', 'RES', 'SHR', 'S', '%CPU', '%MEM', 'TIME+', 'COMMAND'])
-    # upload to g_sheet
-    if j == 1:
+    for i in range(0, 5):
+        file1 = open(f'top{i}.txt', 'r')
+        Lines = file1.readlines()
+        if i == 0:
+            for l in range(3, 5):
+                # print(Lines[l])
+                result.append([Lines[l][:-2], '', '', '', '', '', '', '', '', '', '', ''])
+            result.append(['PID', 'USER', 'PR', 'NI', 'VIRT', 'RES', 'SHR', 'S', '%CPU', '%MEM', 'TIME+', 'COMMAND'])
+        else:
+
+            for j in range(7, len(Lines)):
+                result_list = (Lines[j].split('\n'))[0].split(' ')
+                result_list = list(filter(lambda a: a != '', result_list))
+                result.append(result_list)
+    # print(*result, sep='\n')
+    # print(len(result))
+    # df = pd.DataFrame(result, columns=['PID', 'USER', 'PR', 'NI', 'VIRT', 'RES', 'SHR', 'S', '%CPU', '%MEM', 'TIME+', 'COMMAND'])
+    df = pd.DataFrame(result, columns=['', '', '', '', '', '', '', '', '', '', '', ''])
+    # print(df)
+    if k == 1:
         gsheet_name = create_gsheet()
-    create_worksheet(json_file, gsheet_name, f'S{j}_{hour_min_time_underscore_format()}', 1000, 26)
-    write_df_in_sheet(json_file, gsheet_name, f'S{j}_{hour_min_time_underscore_format()}', df)
-    print('report_num:', j)
-    sleep(3000)
+    create_worksheet(json_file, gsheet_name, f'S{k}_{hour_min_time_underscore_format()}', 1000, 26)
+    write_df_in_sheet(json_file, gsheet_name, f'S{k}_{hour_min_time_underscore_format()}', df)
+    print('report_num:', k)
+    sleep(5)
---
+++ run2.py
@@ -0,0 +1,43 @@
+import os
+from utils.gspread_utils import *
+from datetime import datetime
+from pathlib import Path
+from time import sleep
+
+json_file = Path(__file__).parent / 'credentials/credentials.json'
+
+
+def read_date_time_underscore_format():
+    return str(datetime.now().strftime('%d-%m-%y_%H-%M-%S'))
+
+
+def hour_min_time_underscore_format():
+    return str(datetime.now().strftime('%H-%M'))
+
+
+def create_gsheet():
+    gsheet_name = f'LR_MCR_{read_date_time_underscore_format()}'
+    create_and_share_drive_spreadsheet_for_all_result(gsheet_name, json_file)
+    return gsheet_name
+
+
+gsheet_name = ''
+for j in range(1, 5):
+    command = 'top -b -n 1 > top.txt'
+    os.system(command)
+    # txt to df
+    file1 = open('top.txt', 'r')
+    Lines = file1.readlines()
+    result = []
+    for i in range(7, len(Lines)):
+        result_list = (Lines[i].split('\n'))[0].split(' ')
+        result_list = list(filter(lambda a: a != '', result_list))
+        result.append(result_list)
+    df = pd.DataFrame(result, columns=['PID', 'USER', 'PR', 'NI', 'VIRT', 'RES', 'SHR', 'S', '%CPU', '%MEM', 'TIME+', 'COMMAND'])
+    # upload to g_sheet
+    if j == 1:
+        gsheet_name = create_gsheet()
+    create_worksheet(json_file, gsheet_name, f'S{j}_{hour_min_time_underscore_format()}', 1000, 26)
+    write_df_in_sheet(json_file, gsheet_name, f'S{j}_{hour_min_time_underscore_format()}', df)
+    print('report_num:', j)
+    sleep(3000)
